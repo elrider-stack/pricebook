e4a54f0db10e69a082fb5cc6712bbb3288f69f81
elrider-stack <umasy7@yahoo.com>
2026-07-22 13:03:50 +0100

    Add insert product function

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -4,84 +4,41 @@ import 'package:sqflite/sqflite.dart';
 import '../models/product.dart';
 
 class DatabaseHelper {
-  static final DatabaseHelper instance = DatabaseHelper._init();
+  DatabaseHelper._();
+
+  static final DatabaseHelper instance = DatabaseHelper._();
 
   static Database? _database;
 
-  DatabaseHelper._init();
-
   Future<Database> get database async {
     if (_database != null) return _database!;
-
-    _database = await _initDB('pricebook.db');
+    _database = await _initDatabase();
     return _database!;
   }
 
-  Future<Database> _initDB(String filePath) async {
+  Future<Database> _initDatabase() async {
     final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
 
-    return await openDatabase(path, version: 1, onCreate: _createDB);
-  }
-
-  Future _createDB(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE products(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        price REAL NOT NULL,
-        category TEXT NOT NULL,
-        createdAt TEXT NOT NULL
-      )
-    ''');
+    return openDatabase(
+      join(dbPath, 'pricebook.db'),
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE products(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            price REAL NOT NULL,
+            category TEXT NOT NULL,
+            createdAt TEXT NOT NULL
+          )
+        ''');
+      },
+    );
   }
 
   Future<int> insertProduct(Product product) async {
-    final db = await instance.database;
+    final db = await database;
 
     return await db.insert('products', product.toMap());
-  }
-
-  Future<List<Product>> getAllProducts() async {
-    final db = await instance.database;
-
-    final result = await db.query('products', orderBy: 'name ASC');
-
-    return result.map((e) => Product.fromMap(e)).toList();
-  }
-
-  Future<List<Product>> searchProducts(String keyword) async {
-    final db = await instance.database;
-
-    final result = await db.query(
-      'products',
-      where: 'name LIKE ?',
-      whereArgs: ['%$keyword%'],
-      orderBy: 'name ASC',
-    );
-
-    return result.map((e) => Product.fromMap(e)).toList();
-  }
-
-  Future<int> updateProduct(Product product) async {
-    final db = await instance.database;
-
-    return await db.update(
-      'products',
-      product.toMap(),
-      where: 'id = ?',
-      whereArgs: [product.id],
-    );
-  }
-
-  Future<int> deleteProduct(int id) async {
-    final db = await instance.database;
-
-    return await db.delete('products', where: 'id = ?', whereArgs: [id]);
-  }
-
-  Future<void> close() async {
-    final db = await instance.database;
-    await db.close();
   }
 }
