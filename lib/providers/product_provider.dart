eb0073d47e59d52ab14ed9f11ffbdcd8cf23fc20
elrider-stack <umasy7@yahoo.com>
2026-07-22 15:36:47 +0100

    Add product sorting provider

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -3,31 +3,70 @@ import 'package:flutter/material.dart';
 import '../database/database_helper.dart';
 import '../models/product.dart';
 
-class ProductProvider extends ChangeNotifier {
-  final DatabaseHelper _databaseHelper = DatabaseHelper.instance;
+enum SortType { newest, oldest, name, priceLow, priceHigh }
 
-  List<Product> _products = [];
+class ProductProvider extends ChangeNotifier {
   List<Product> _allProducts = [];
+  List<Product> _products = [];
+
+  SortType _sortType = SortType.newest;
 
   List<Product> get products => _products;
 
-  int get totalProducts => _allProducts.length;
+  int get totalProducts => _products.length;
+
+  SortType get sortType => _sortType;
 
   Future<void> loadProducts() async {
-    _allProducts = await _databaseHelper.getProducts();
+    _allProducts = await DatabaseHelper.instance.getProducts();
     _products = List.from(_allProducts);
+
+    sortProducts(_sortType, notify: false);
+
     notifyListeners();
   }
 
-  void search(String keyword) {
-    if (keyword.trim().isEmpty) {
+  void search(String query) {
+    if (query.isEmpty) {
       _products = List.from(_allProducts);
     } else {
       _products = _allProducts.where((product) {
-        return product.name.toLowerCase().contains(keyword.toLowerCase());
+        return product.name.toLowerCase().contains(query.toLowerCase());
       }).toList();
     }
 
+    sortProducts(_sortType, notify: false);
+
     notifyListeners();
+  }
+
+  void sortProducts(SortType type, {bool notify = true}) {
+    _sortType = type;
+
+    switch (type) {
+      case SortType.newest:
+        _products.sort((a, b) => b.id!.compareTo(a.id!));
+        break;
+
+      case SortType.oldest:
+        _products.sort((a, b) => a.id!.compareTo(b.id!));
+        break;
+
+      case SortType.name:
+        _products.sort((a, b) => a.name.compareTo(b.name));
+        break;
+
+      case SortType.priceLow:
+        _products.sort((a, b) => a.price.compareTo(b.price));
+        break;
+
+      case SortType.priceHigh:
+        _products.sort((a, b) => b.price.compareTo(a.price));
+        break;
+    }
+
+    if (notify) {
+      notifyListeners();
+    }
   }
 }
